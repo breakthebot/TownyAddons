2a78ddba20d2adb692688f70af1d2b76c5f85ebb
Veyronity <veyronitymc@gmail.com>
2025-07-28 22:29:24 +0300

    Fix player null exception, remove perm overrides when town trust removed or plot changed from vault to else

--- src/main/java/org/breakthebot/townyAddons/plot/Vault.java
+++ src/main/java/org/breakthebot/townyAddons/plot/Vault.java
@@ -21,6 +21,7 @@ import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.event.TownBlockTypeRegisterEvent;
 import com.palmergames.bukkit.towny.event.plot.PlayerChangePlotTypeEvent;
 import com.palmergames.bukkit.towny.event.town.TownTrustAddEvent;
+import com.palmergames.bukkit.towny.event.town.TownTrustRemoveEvent;
 import com.palmergames.bukkit.towny.exceptions.TownyException;
 import com.palmergames.bukkit.towny.object.*;
 import com.palmergames.bukkit.towny.utils.PermissionGUIUtil;
@@ -76,7 +77,7 @@ public class Vault implements Listener {
         if (!(newResident instanceof Player)) { return; }
         Player player = newResident.getPlayer();
 
-        assert player != null;
+        if (player == null || !player.isOnline()) { return; }
         if (player.hasPermission("towny.command.townyadmin.town.trust")) { return; }
 
         Town town = event.getTown();
@@ -103,6 +104,28 @@ public class Vault implements Listener {
     }
 
     @EventHandler
+    public void onResidentUntrusted(TownTrustRemoveEvent event) {
+        Resident removedRes = event.getTrustedResident();
+        if (!(removedRes instanceof Player)) { return; }
+        Town town = event.getTown();
+        for (TownBlock townBlock : town.getTownBlocks()) {
+
+            if (townBlock.getType() == null || !"vault".equalsIgnoreCase(townBlock.getType().getName())) {
+                continue;
+            }
+            Map<Resident, PermissionData> overrides = new HashMap<>(townBlock.getPermissionOverrides());
+            overrides.remove(removedRes);
+            townBlock.setPermissionOverrides(overrides);
+
+            try {
+                townBlock.save();
+            } catch (Exception e) {
+                getLogger().severe("Error while saving vault townblock at " + townBlock.getWorldCoord() + "\n" + e);
+            }
+        }
+    }
+
+    @EventHandler
     public void onTownyLoadTownBlockTypes(TownBlockTypeRegisterEvent event) {
         registerVaultPlot();
     }
@@ -110,24 +133,33 @@ public class Vault implements Listener {
     @EventHandler
     public void onChangePlotType(PlayerChangePlotTypeEvent event) {
         TownBlockType newType = event.getNewType();
-        if (newType.getName().equalsIgnoreCase("vault")) {
-            TownBlock vaultBlock = event.getTownBlock();
+        TownBlockType oldType = event.getOldType();
+        TownBlock vaultBlock = event.getTownBlock();
+        Town town = vaultBlock.getTownOrNull();
+        if (town == null) { return ; }
+        Set<Resident> trustedList = town.getTrustedResidents();
+        Map<Resident, PermissionData> overrides = new HashMap<>(vaultBlock.getPermissionOverrides());
+
+        if (oldType.getName().equals("vault")) {
+            for (Resident trusted : trustedList) {
+                overrides.remove(trusted);
+            }
+        }
+        else if (newType.getName().equalsIgnoreCase("vault")) {
             PermissionData denyAll = new PermissionData(negatedPermissions, "VaultSecurity");
-            Town town = vaultBlock.getTownOrNull();
-            if (town == null) { return ; }
-            Set<Resident> trustedList = town.getTrustedResidents();
-            Map<Resident, PermissionData> overrides = new HashMap<>(vaultBlock.getPermissionOverrides());
+
             for (Resident trusted : trustedList) {
                 overrides.put(trusted, denyAll);
             }
-            vaultBlock.setPermissionOverrides(overrides);
+        }
+        else { return; }
 
-            try {
-                vaultBlock.save();
-            } catch (Exception e) {
-                TownyMessaging.sendErrorMsg(event.getPlayer(), "Unexpected error occurred. Please report immediately");
-                getLogger().severe("Error while saving vault townblock at " + vaultBlock.getWorldCoord() + "\n" + e);
-            }
+        vaultBlock.setPermissionOverrides(overrides);
+        try {
+            vaultBlock.save();
+        } catch (Exception e) {
+            TownyMessaging.sendErrorMsg(event.getPlayer(), "Unexpected error occurred. Please report immediately");
+            getLogger().severe("Error while saving vault townblock at " + vaultBlock.getWorldCoord() + "\n" + e);
         }
     }
 }
